06e15b3015f016352b8fa2bab5c47981750387eb
Mihai Procopi <37700827+thatc0py@users.noreply.github.com>
2025-05-01 14:26:02 +0400

    Update tmux-sessionizer.sh

--- bin/tmux-sessionizer.sh
+++ bin/tmux-sessionizer.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-search_dir=(
-    # Add serached directories
-)
+search_dir=(~/personal ~/thatc0py ~/Downloads ~/.config/nvim)
 
 if [[ $# -eq 1 ]]; then
     selected=$1
@@ -12,28 +10,27 @@ else
     selected=$(echo "$paths" | fzf --reverse --prompt="Select directory: " --height=16 --scheme=path +m)
 fi
 
-if [[ -z $selected ]]; then
+if [[ -z "$selected" ]]; then
     exit 0
 fi
 
 child_folder=$(basename "$selected")
 parent_folder=$(basename $(dirname "$selected"))
 
-# selected_name=$(basename "$selected" | tr . _)
 selected_name=$(echo "$parent_folder~$child_folder" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
+    tmux new-session -s "$selected_name" -c "$selected"
     exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
 if [[ -z $TMUX ]]; then
-    tmux attach -t $selected_name
+    tmux attach -t "$selected_name"
 else
-    tmux switch-client -t $selected_name
+    tmux switch-client -t "$selected_name"
 fi
